1ea22fc15005b79213c9faafc996f8bf8d817859
Emily <emily.stanisha.17@cnu.edu>
2019-10-28 10:45:48 -0400

    Wrote file in/out for proj

--- FileIO.cpp
+++ FileIO.cpp
@@ -13,12 +13,40 @@ using namespace std;
 
 int KP_FileIO::getFileContents(const std::string &filename, std::string &contents)
 {
+	ifstream myfile;
+	myfile.open(filename, ios::in);
+
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_READ;
+	}
+
+	string line = "";
+
+	while (getline(myfile, line)) {
+		getline(myfile, line);
+		contents += line;
+	}
+
+	return SUCCESS;
+
 	//TODO fill in
 }
 
 int KP_FileIO::writeVectortoFile(const std::string filename,std::vector<std::string> &myEntryVector)
 {
-	//TODO fill in
+	ofstream myfile;
+	myfile.open(filename);
+
+	if (!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE_TO_WRITE;
+	}
+
+	for (int i = 0; i < myEntryVector.size(); i++) {
+		myfile << myEntryVector[i] << "\n";
+	}
+
+	return SUCCESS;
+
 }
 
 
